0bb4cc0573c07c28efd57f035ffe81db81881d7d
zhuyongqiang <zhuyongqiang@kingsoft.com>
2018-06-28 17:13:07 +0800

    添加kafka启动步骤 先启动zookeeper，然后启动kafka，创建主题，在主题下服务者发送消息，消费者接收消息。

--- src/main/java/com/dragon/KafkaLearningApplication.java
+++ src/main/java/com/dragon/KafkaLearningApplication.java
@@ -16,6 +16,25 @@ import org.springframework.context.ConfigurableApplicationContext;
  * <version>2.1.3.RELEASE</version>
  * </dependency>
  */
+
+/**
+ * kafka启动步骤
+ * 1、在zookeeper目录下启动zookeeper
+ *    ./zkServer.sh start
+ * 2、后台启动kafka命令
+ *    ./kafka-server-start.sh ../config/server.properties 1>/dev/null 2>&1 &
+ * 启动后，查看进程 ，使用jps命令会显示：Jps QuorumPeerMain，启动之后会出现kafka ，启动服务者和消费者后，出现ConsoleProducer，ConsoleConsumer
+ * 3、创建主题
+ * ./kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+ * 4、查看主题列表
+ * ./kafka-topics.sh --list --zookeeper localhost:2181
+ * 5、服务者发送消息
+ * ./kafka-console-producer.sh --broker-list 192.168.83.150:9092 --topic test
+ *   this is message
+ * 6、消费者接收消息
+ * ./kafka-console-consumer.sh --bootstrap-server 192.168.83.150:9092 --topic test --from-beginning
+ *   this is message
+ */
 @SpringBootApplication
 public class KafkaLearningApplication {
 
